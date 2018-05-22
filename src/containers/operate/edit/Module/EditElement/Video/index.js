83e8cb59d8c33c387179be01da45466cd77a3236
alongzjl <m18251132450@163.com>
2018-05-22 11:55:47 +0800

    1，去除视频组件显示的控制项 2，修复视频上传的bug（与图片保持一致的格式）

--- src/containers/operate/edit/Module/EditElement/Video/index.js
+++ src/containers/operate/edit/Module/EditElement/Video/index.js
@@ -16,14 +16,14 @@ class Video extends React.Component {
 
 	render() {
 		let { data } = this.props
-		// console.log(1, this.props)
+		const content = data.data.content
 		return (
 			<div className="e-video">
-				<video src={data.data.content.video || data.data.content.src} controls="controls">
-					您的浏览器不支持 video 标签。
-				</video>
-			</div>    
-		) 
+				<video src={content.video.video || content.src} controls={false} autoPlay>  
+					您的浏览器不支持 video 标签。  
+				</video> 
+			</div>     
+		)  
 	}
 } 
 
