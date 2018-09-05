7432c087d1ea1d40f70a3e3fb4e6de42c0052d11
jimmy.zhuang <zhuangjiaqi@rongyi.com>
2018-09-05 16:26:46 +0800

    完成 mock数据 模块修改 作品编辑器完成

--- src/containers/operate/edit/Module/EditElement/StoreList2/Page/index.js
+++ src/containers/operate/edit/Module/EditElement/StoreList2/Page/index.js
@@ -11,13 +11,6 @@ export default class PageByStore2 extends React.Component {
 	// constructor(props) {
 	// 	super(props)
 	// }
-	onChange = (e, item) => {
-		e.stopPropagation()
-		let { ioInput, ioOuter } = this.props,
-			{ body } = ioInput
-		body.page = item.name
-		ioOuter(ioInput)
-	}
 	renderItem = (item, page, idx) => {
 		let { data } = this.props,
 			{ name } = item,
@@ -42,7 +35,7 @@ export default class PageByStore2 extends React.Component {
 		let { body } = this.props.ioInput
 		let list = numArr
 		return list.map((_, i) => {
-			return <div key={i} onClick={e => this.onChange(e, _)}>{this.renderItem(_, body.page, i)}</div>
+			return <div key={i}>{this.renderItem(_, body.page, i)}</div>
 		})
 	}
 	render() {
